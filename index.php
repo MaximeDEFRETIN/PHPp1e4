00f1a344be0bac62aedd461c0abb6616d6fdde61
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-13 08:35:17 +0100

    Correction

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
-<?php
-$string = "Fus Ro Dah";
-$int = 36000;
-$boleen = true;
-$float = 1.618;
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Partiee 1 : exercice 4</title>
+  </head>
+  <body>
+    <?php
+    $string = 'Fus Ro Dah';
+    $int = 36000;
+    $boleen = true;
+    $float = 1.618;
 
-echo "$string $int $boleen $float";
-?>
+    echo "$string $int $boleen $float";
+    ?>
+  </body>
+</html>
